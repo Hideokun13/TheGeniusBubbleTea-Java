6cd1b347fdd2dbdae7ffda19fbc08ef2787504a6
Hideokun13 <boypeerawit@hotmail.com>
2021-01-07 16:59:39 +0700

    Added welcome screen

---
+++ TheGeniusBubbleTea/src/com/HideoDK/Main.java
@@ -0,0 +1,17 @@
+package com.HideoDK;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DisplayWelcome();
+    }
+    private static void DisplayWelcome(){
+        System.out.println("==================================================================\n" +
+                "|                                                                 |\n" +
+                "|                                                                 |\n" +
+                "|                 Welcome to The Genius Bubble Tea                |\n" +
+                "|                                                                 |\n" +
+                "|                                                                 |\n" +
+                "==================================================================\n");
+    }
+}
