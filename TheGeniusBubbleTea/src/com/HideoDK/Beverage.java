3bb115dab865f158e18887d6aae434ca9d1a4469
Hideokun13 <boypeerawit@hotmail.com>
2021-01-07 20:27:50 +0700

    Create Beverage.java

---
+++ TheGeniusBubbleTea/src/com/HideoDK/Beverage.java
@@ -0,0 +1,19 @@
+package com.HideoDK;
+
+public class Beverage {
+    private String drinkName;
+    private int drinkPrice;
+    private String topping;
+    private int priceTop;
+    private String SweetLv;
+    private String size;
+    private int PriceSize;
+
+    public Beverage(){
+
+    }
+    public Beverage(String drinkName, int drinkPrice){
+        this.drinkName = drinkName;
+        this.drinkPrice = drinkPrice;
+    }
+}
